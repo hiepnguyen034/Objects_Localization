5d184fe1ff656aee4c2baf91143d078956431764
hiepnguyen034 <hiepnguyen034@gmail.com>
2018-11-06 11:27:11 -0500

    First commit

---
+++ YOLO_process.py
@@ -0,0 +1,92 @@
+import argparse
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import PIL
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Input, Lambda, Conv2D
+from keras.models import load_model, Model
+from yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
+from yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
+
+
+def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold = .6):
+    
+    box_scores = box_confidence * box_class_probs
+    #Find the box_classes thanks to the max box_scores, keep track of the corresponding score
+   
+    box_classes = tf.argmax(box_scores,axis=-1)
+    box_class_scores = tf.reduce_max(box_scores,axis=-1)   
+    # Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
+    # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
+    filtering_mask = tf.greater_equal(box_class_scores, threshold)
+    
+    # Apply the mask to scores, boxes and classes
+    scores = tf.boolean_mask(box_class_scores,filtering_mask)
+    boxes = tf.boolean_mask(boxes,filtering_mask)
+    classes = tf.boolean_mask(box_classes,filtering_mask)
+    
+    return scores, boxes, classes
+
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_threshold = 0.5):
+
+    
+    max_boxes_tensor = tf.Variable(max_boxes, dtype='int32')     # tensor to be used in tf.image.non_max_suppression()
+    K.get_session().run(tf.variables_initializer([max_boxes_tensor])) # initialize variable max_boxes_tensor
+    
+    # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
+    nms_indices = tf.image.non_max_suppression(boxes,scores,max_boxes,iou_threshold=iou_threshold)
+    scores = tf.gather(scores,nms_indices)
+    boxes = tf.gather(boxes,nms_indices)
+    classes = tf.gather(classes,nms_indices)
+    
+    return scores, boxes, classes
+
+
+def yolo_eval(yolo_outputs, image_shape = (720., 1280.), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+    box_confidence, box_xy, box_wh, box_class_probs = yolo_outputs
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
+    scores, boxes, classes = yolo_filter_boxes(box_confidence,boxes,box_class_probs)
+    boxes = scale_boxes(boxes, image_shape)
+    scores, boxes, classes = yolo_non_max_suppression(scores,boxes,classes)    
+    return scores, boxes, classes
+
+
+
+def predict(sess, image_file):
+    # Preprocess your image
+    #sess=get_session_data()	
+    # yolo_model = load_model("model_data/yolo.h5")
+    # scores, boxes, classes = load_yolo_model()
+    class_names = read_classes("model_data/coco_classes.txt")
+    anchors = read_anchors("model_data/yolo_anchors.txt")
+    image_shape = (720., 1280.)
+    yolo_model = load_model("model_data/yolo.h5")
+    yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
+    scores, boxes, classes = yolo_eval(yolo_outputs, image_shape)
+    image, image_data = preprocess_image("images/" + image_file, model_image_size = (608, 608))
+
+    # Run the session with the correct tensors and choose the correct placeholders in the feed_dict.
+    out_scores, out_boxes, out_classes = sess.run([scores,boxes,classes],feed_dict={
+    	yolo_model.input: image_data, K.learning_phase(): 0
+    })
+
+    print('Found {} boxes for {}'.format(len(out_boxes), image_file))
+    colors = generate_colors(class_names)
+    draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors)
+    image.save(os.path.join("out", image_file), quality=90)
+    output_image = scipy.misc.imread(os.path.join("out", image_file))
+    imshow(output_image)
+    
+    return out_scores, out_boxes, out_classes
+
+sess = K.get_session()
+
+if __name__ == '__main__':
+	predict(sess,"test.jpg")
